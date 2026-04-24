00415299a05a43af4b6481c16e6cc2ace4109dbf
Samuel de Angeli Rodrigues <110358743+SamueldeAngeli@users.noreply.github.com>
2026-04-24 17:01:39 +0000

    fix: add express-session dependency

--- web/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const apiController = require('../controllers/apiController');
-
-const router = express.Router();
-
-router.get('/health', apiController.health);
-router.post('/fivem/logs', apiController.receiveFiveMLog);
-
-module.exports = router;
